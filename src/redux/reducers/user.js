426421569871e6aa9781e81106534fee291de52a
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-04-11 18:03:15 -0300

    redux setup

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReduce = (state = INITIAL_STATE, action) => {
+  switch (action.type){
+    default: return INITIAL_STATE;
+  }
+};
+
+export default userReduce;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  temporaryHolder: 'Temporary Holder',
+};
+
+const walletReduce = (state = INITIAL_STATE, action) => {
+  switch (action.type){
+    default: return INITIAL_STATE;
+  }
+};
+
+export default walletReduce;
